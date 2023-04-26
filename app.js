89c5ab69ec7a57174e1c8cfc027f839d7004f987
Steven Ndegwa <stevencallistus19@gmail.com>
2023-04-26 12:57:56 +0300

    adding rooms

--- app.js
+++ app.js
@@ -30,29 +30,34 @@ app.get('/chat',(req,res)=>{
 })
 
 let user = "";
+let room = "";
 
 app.post('/',(req,res)=>{
     user = req.body.name;
+    room = req.body.room;
     res.redirect("/chat");
 })
 
 io.on('connection',socket=>{
+    let room2 = room;
     let user2 = user;
     console.log(`${user2} joined the chart`);
     socket.emit('message',`${user2} connected on Socket ${socket.id}`);
     socket.emit('message',`welcome to the chart ${user2}`); // to the client
 
-    socket.broadcast.emit('message',`${user} joined the chart`); // all clients but user
+    socket.join(room2);
+    
+    socket.broadcast.to(room2).emit('message',`${user} joined the chart`); // all clients but user
     // io.emit(); //all clients
 
 
     socket.on('chatMessage',(msg)=>{
-        socket.broadcast.emit('chatMessage',msg,user2);
+        socket.broadcast.to(room2).emit('chatMessage',msg,user2);
     })
 
 
     socket.on('disconnect',()=>{
-        io.emit('message',`${user2} left the chart`);
+        io.to(room2).emit('message',`${user2} left the chart`);
     })
 
 })
